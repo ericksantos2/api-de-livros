ee00bced880969eb445056bd7e0c9b947344ef66
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-03-24 11:50:18 -0300

    primeiro commit

---
+++ rotas/livro.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  try {
+    res.send('Olá mundo da Alura do Brasil!');
+  } catch (err) {
+    res.status(500);
+    res.send(error.message);
+  }
+})
+
+router.post('/', (req, res) => {
+  res.send('Você fez uma requisição do tipo POST');
+})
+router.patch('/', (req, res) => {
+  res.send('Você fez uma requisição do tipo PATCH');
+})
+router.delete('/', (req, res) => {
+  res.send('Você fez uma requisição do tipo DELETE');
+})
+
+module.exports = router;
