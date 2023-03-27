77dc75353c32b060f9dec6e55648930e102dae6f
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-03-27 11:41:33 -0300

    movi a parte em que é realizada a leitura do arquivo livros.json para um outro arquivo, e tambem adicionei a função de procurar um livro por ID

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,9 +1,19 @@
-const fs = require('fs');
+const { getTodosLivros, getLivroPorId } = require('../servicos/livro');
 
 function getLivros(req, res) {
   try {
-    const livros = JSON.parse(fs.readFileSync('livros.json'));
+    const livros = getTodosLivros();
     res.send(livros);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+}
+function getLivro(req, res) {
+  try {
+    const id = req.params.id;
+    const livro = getLivroPorId(id);
+    res.send(livro);
   } catch (error) {
     res.status(500);
     res.send(error.message);
@@ -11,5 +21,6 @@ function getLivros(req, res) {
 }
 
 module.exports = {
-  getLivros
+  getLivros,
+  getLivro
 }
